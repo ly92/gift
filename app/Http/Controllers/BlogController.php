647071c36d134d0417f4dab11d69643cbbe2f738
liyong <liyong@7xiaofu.com>
2019-01-08 17:52:00 +0800

    fix bug

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,7 +9,7 @@ use App\Models\Post;
 class BlogController extends Controller
 {
     public function index(){
-        $post = Post::where('published_at', '<=', Carbon::now())->orderBy('published_at', 'desc')->paginate(config('blog.posts_per_page'));
+        $posts = Post::where('published_at', '<=', Carbon::now())->orderBy('published_at', 'desc')->paginate(config('blog.posts_per_page'));
         return view('blog.index', compact('posts'));
     }
 
