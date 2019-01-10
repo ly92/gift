f8461203766d6e110b46fdce080f99e7385b9d08
liyong <liyong@7xiaofu.com>
2019-01-10 13:39:35 +0800

    tag

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TagController extends Controller
 {
+
+
+    protected $fields = [
+        'tag' => '',
+        'title' => '',
+        'subtitle' => '',
+        'meta_description' => '',
+        'page_image' => '',
+        'layout' => 'blog.layouts.index',
+        'reverse_direction' => 0,
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +27,8 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::all();
+        return view('admin.tag.index')->withTags($tags);
     }
 
     /**
@@ -24,7 +38,11 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
+        $data = [];
+        foreach ($this->fields as $field => $default){
+            $data[$field] = old($field, $default);
+        }
+        return view('admin.tag.create', $data);
     }
 
     /**
@@ -35,7 +53,13 @@ class TagController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $tag = new Tag();
+        foreach (array_keys($this->fields) as $field){
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+
+        return redirect('/admin/tag')->with('success', '标签[' . $tag->tag . ']创建成功.');
     }
 
     /**
@@ -57,7 +81,13 @@ class TagController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        $data = ['id' => $id];
+        foreach (array_keys($this->fields) as $field){
+            $data[$field] = old($field, $tag->$field);
+        }
+
+        return view('admin.tag.edit', $data);
     }
 
     /**
@@ -69,7 +99,13 @@ class TagController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        foreach (array_keys(array_except($this->fields, ['tag'])) as $field){
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+
+        return redirect('/admin/tag/' . $id . '/edit')->with('success', '修改已保存.');
     }
 
     /**
@@ -80,6 +116,9 @@ class TagController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        $tag->delete();
+
+        return redirect('/admin/tag')->with('success', '标签「' . $tag->tag . '」已经被删除.');
     }
 }
