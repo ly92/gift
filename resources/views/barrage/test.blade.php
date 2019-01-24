3c2669e8cd7a392008c688f2665755ebed544934
liyong <liyong@7xiaofu.com>
2019-01-24 13:44:14 +0800

    test

--- resources/views/barrage/test.blade.php
+++ resources/views/barrage/test.blade.php
@@ -1,21 +1,25 @@
-<!DOCTYPE html>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Device Example</title>
-    <script type="text/javascript">
-        // H5 plus事件处理
-        function plusReady(){
-            alert( "uuid: "+plus.device.uuid );
-        }
-        if(window.plus){
-            plusReady();
-        }else{
-            document.addEventListener("plusready",plusReady,false);
-        }
-    </script>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <title>javascript获取访问者设备信息</title>
 </head>
+
 <body>
-获取设备的唯一标识号
+<script type="text/javascript">
+    document.write("userAgent: " + navigator.userAgent + "<br><br>");
+    document.write("appName: " + navigator.appName + "<br><br>");
+    document.write("appCodeName: " + navigator.appCodeName + "<br><br>");
+    document.write("appVersion: " + navigator.appVersion + "<br><br>");
+    document.write("appMinorVersion: " + navigator.appMinorVersion + "<br><br>");
+    document.write("platform: " + navigator.platform + "<br><br>");
+    document.write("cookieEnabled: " + navigator.cookieEnabled + "<br><br>");
+    document.write("onLine: " + navigator.onLine + "<br><br>");
+    document.write("userLanguage: " + navigator.language + "<br><br>");
+    document.write("mimeTypes.description: " + navigator.mimeTypes[1].description + "<br><br>");
+    document.write("mimeTypes.type: " + navigator.mimeTypes[1].type + "<br><br>");
+    document.write("plugins.description: " + navigator.plugins[3].description + "<br><br>");
+</script>
 </body>
 </html>
